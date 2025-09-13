a52edece2162fd6944d4129f17c5e09551005239
JelloPoint <developer@jellopoint.com>
2025-09-13 17:32:57 +0200

    Update phpstan.neon en create constants.php

---
+++ stubs/constants.php
@@ -0,0 +1,8 @@
+<?php
+// Loaded only by PHPStan (not shipped in the plugin).
+if (!defined('JPRM_PLUGIN_PATH')) {
+    define('JPRM_PLUGIN_PATH', '');
+}
+if (!defined('JPRM_PLUGIN_URL')) {
+    define('JPRM_PLUGIN_URL', '');
+}
