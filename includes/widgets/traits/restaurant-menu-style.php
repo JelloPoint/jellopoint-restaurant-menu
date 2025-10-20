bc115eadee92d1398646771ade6472edf567cf4e
JelloPoint <developer@jellopoint.com>
2025-10-20 11:57:39 +0200

    Update restaurant-menu-style.php

--- includes/widgets/traits/restaurant-menu-style.php
+++ includes/widgets/traits/restaurant-menu-style.php
@@ -11,6 +11,11 @@ trait Restaurant_Menu_Style {
 			'label'=>__('Menu Title & Description','jellopoint-restaurant-menu'),
 			'tab'=>\Elementor\Controls_Manager::TAB_STYLE,
 		]);
+		$this->add_control( 'jprm_curr_heading', [
+			'label'     => __( 'Currency', 'jellopoint-restaurant-menu' ),
+			'type'      => Controls_Manager::HEADING,
+			'separator' => 'before',
+		] );
 		$this->add_group_control(\Elementor\Group_Control_Typography::get_type(),[
 			'name'=>'menu_title_typo',
 			'global'=>['default'=>\Elementor\Core\Kits\Documents\Tabs\Global_Typography::TYPOGRAPHY_PRIMARY],
