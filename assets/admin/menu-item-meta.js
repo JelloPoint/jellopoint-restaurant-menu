7f21ac855659c773050d8c03b7631efbef3289a5
JelloPoint <developer@jellopoint.com>
2025-10-21 20:43:30 +0200

    Again restore 1

--- assets/admin/menu-item-meta.js
+++
@@ -1,6 +0,0 @@
-/* JelloPoint Menu Item Admin – placeholder hook
-   Core behaviors are handled inline in class-admin-menuitem-meta.php
-*/
-(function($){
-  // Reserved for future enhancements; no-op by default.
-})(jQuery);
