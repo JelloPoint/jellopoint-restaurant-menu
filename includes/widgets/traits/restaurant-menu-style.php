55b8ad1c47cbb947c57d01530a1884821ef405f2
JelloPoint <developer@jellopoint.com>
2025-11-04 19:49:46 +0100

    Update restaurant-menu-style.php

--- includes/widgets/traits/restaurant-menu-style.php
+++ includes/widgets/traits/restaurant-menu-style.php
@@ -204,7 +204,7 @@ trait Restaurant_Menu_Style {
 				'type'       => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', 'em', '%' ],
 				'selectors' => [
-					'{{WRAPPER}} .jp-menu__section--level-0 > .jp-section__title' => 'text-align: {{VALUE}};',
+					'{{WRAPPER}} .jp-menu__section--level-0' => 'text-align: {{VALUE}};',
 				],
 			]
 		);
